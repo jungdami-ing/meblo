a23bf2780dddd4394bdb80f36b0b36c963eea9f4
jungdami-ing <jungdamiing@gmail.com>
2021-07-13 15:15:53 +0900

    환경세팅

---
+++ src/main/java/com/meblo/blog/MebloApplication.java
@@ -0,0 +1,13 @@
+package com.meblo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MebloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MebloApplication.class, args);
+	}
+
+}
